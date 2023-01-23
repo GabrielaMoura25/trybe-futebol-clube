f8ef1163adfdc98ab406a29020375710f9a3fae2
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-01-23 17:16:50 -0300

    criei o user controller e implementei

---
+++ app/backend/src/controllers/UserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import ILogin from '../interfaces/ILogin';
+import UserService from '../services/UserService';
+
+export default class UserController {
+  constructor(private _model = new UserService()) {}
+
+  async login(req: Request, res: Response) {
+    const user = req.body as ILogin;
+
+    const { code, message, token } = await this._model.login(user);
+
+    if (message) return res.status(code).json({ message });
+
+    return res.status(code).json({ token });
+  }
+
+  async validateLogin(req: Request, res: Response) {
+    const { authorization } = req.headers;
+
+    const { code, message, role } = await this._model.validateLogin(authorization);
+
+    if (message) return res.status(code).json({ message });
+
+    return res.status(code).json({ role });
+  }
+}
